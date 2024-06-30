b345cb0a91f869ffd1408a2af9edccc999a9ca02
Zaid Sharieff <zaidsharieff09@gmail.com>
2024-06-30 22:35:20 +0530

    session2: fix color values

--- block_shooter.py
+++ block_shooter.py
@@ -23,8 +23,8 @@ pygame.display.set_caption('game')
 
 red = (255, 0, 0)
 blue = (0, 0, 255)
-white = (0, 0, 0)
-black = (255, 255, 255)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 player_x = 500
 player_y = 630
